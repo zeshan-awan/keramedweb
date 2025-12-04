9b510c8038044a953e512c9384a7b218806f98a7
Zeshan <zeshan.uhy97@gmail.com>
2025-12-04 11:58:49 +0500

    header was setted

--- app.js
+++ app.js
@@ -112,3 +112,20 @@ setInterval(() => {
   currentIndex++;
   snapToSlide();
 }, 3000);
+
+
+
+
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-x-full");
+    overlay.classList.toggle("hidden");
+  });
+
+  overlay.addEventListener("click", () => {
+    sidebar.classList.add("-translate-x-full");
+    overlay.classList.add("hidden");
+  });
